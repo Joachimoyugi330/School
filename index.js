501daf4a07aef16e015d836cb24cd41f5ad8043a
Joachim Oyugi <joachimoyugi@gmail.com>
2024-07-09 17:02:41 +0300

    Added Student Crud Operations

--- index.js
+++ index.js
@@ -10,9 +10,84 @@ const courses =[
     {id: 3, name: 'English'}
 ]
 
-app.get('/', (req, res)=>{
-    res.send('Hello World');
+// Students section
+const students = [{
+    adm: 100,
+    name: "Joachim",
+    email: "joachimoyugi@gmail.com"
+},
+{
+adm: 101,
+name: "Mary",
+email: "maryg@gmail.com"
+}
+]
+
+app.get('/api/students', (req, res)=>{
+    res.send(students);
 });
+
+app.get('/api/student/:adm', (req, res)=>{
+    const student = students.find(student => student.adm === parseInt(req.params.adm))
+
+    if(!student) return res.status(404).send("Student with that ID does not exist")
+    
+    res.send(student);
+})
+
+app.post('/api/students', (req, res)=>{
+    const { error } = validateStudent(req.body)
+
+    if(error) return res.status(400).send(error.details[0].message)
+
+    const new_adm = (students[students.length-1].adm)+1
+    const student = {
+        adm: new_adm,
+        name: req.body.name,
+        email: req.body.email
+    };
+
+    students.push(student);
+    res.send(student);
+});
+
+app.put('/api/student/:adm', (req, res)=>{
+    const student = students.find( student => student.adm===parseInt(req.params.adm));
+    console.log(student)
+    if(!student) return res.status(404).send(`Student with adm: ${req.params.adm} does not exist`);
+    
+    const { error } = validateStudent(req.body);
+
+    if(error) return res.status(400).send(error.details[0].message);
+    
+    student.name = req.body.name
+    student.email = req.body.email
+
+    res.send(student);
+});
+
+app.delete('/api/student/:adm', (req, res)=>{
+    const student = students.find( student => student.adm===parseInt(req.params.adm));
+    if(!student) return res.status(404).send("Student with adm does not exist");
+
+    const index = students.indexOf(student)
+    students.splice(index, 1);
+    res.send(student);
+})
+
+// Validate Function
+function validateStudent(course){
+    const schema = {
+        name: Joi.string().required(),
+        email: Joi.string().min(5).required()
+    }
+    return Joi.validate(course, schema)
+}
+
+// Students section
+
+
+
 
 app.get('/api/courses', (req, res)=>{
     res.send(courses)
